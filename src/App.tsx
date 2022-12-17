40490f72d1cd485c74f1ea6b4c93f6fa9fc7bdb0
ismailbenlaredj <yako_is@yahoo.fr>
2022-12-17 10:20:43 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler'
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const App = () => (
+  <View>
+    <Text>App</Text>
+  </View>
+)
+
+export default App
